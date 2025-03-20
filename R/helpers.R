a5cd44d699addb40fc796cfc75910e1f022d1eda
Anton Hung <amh2389@ip-172-25-96-227.ec2.internal>
2025-03-20 18:14:26 +0000

    reorganize file structure

---
+++ R/helpers.R
@@ -0,0 +1,86 @@
+# -------------------------------------
+# Script: helpers.R
+# Author: Nick Williams
+# Updated:
+# Purpose: Functions for repeat tasks
+# Notes:
+# -------------------------------------
+
+library(arrow)
+library(fst)
+
+write_data <- function(data, file, dir = "/mnt/general-data/disability/everything-local-lmtp") {
+  write_fst(data, file.path(dir, file))
+}
+
+load_data <- function(file) {
+  dir <- "/mnt/general-data/disability/everything-local-lmtp"
+  read_fst(file.path(dir, file))
+}
+
+#' Open an arrow dataset for the dates files
+open_dedts <- function(path = "/mnt/processed-data/disability") {
+  list.files(path, 
+             pattern = "TAFDEDTS_\\d+\\.parquet", 
+             recursive = TRUE) |> 
+    (\(files) file.path(path, files))() |> 
+    open_dataset()
+}
+
+#' Open an arrow dataset for the inpatient hospital files
+open_iph <- function(path = "/mnt/processed-data/disability") {
+  list.files(path, 
+             pattern = "TAFIPH_\\d+\\.parquet", 
+             recursive = TRUE) |> 
+    (\(files) file.path(path, files))() |> 
+    open_dataset()
+}
+
+#' Open an arrow dataset for the other services files
+open_oth <- function(path = "/mnt/processed-data/disability") {
+  list.files(path, 
+             pattern = "TAFOTH\\d+_\\d+\\.parquet", 
+             recursive = TRUE) |> 
+    (\(files) file.path(path, files))() |> 
+    open_dataset()
+}
+
+open_otl <- function(path = "/mnt/processed-data/disability/") {
+  list.files(path, 
+             pattern = "TAFOTL\\d+_\\d+\\.parquet", 
+             recursive = TRUE) |> 
+    (\(files) file.path(path, files))() |> 
+    open_dataset()
+}
+
+open_rxl <- function(path = "/mnt/processed-data/disability/") {
+  list.files(path, 
+             pattern = "TAFRXL_\\d+\\.parquet", 
+             recursive = TRUE) |> 
+    (\(files) file.path(path, files))() |> 
+    open_dataset()
+}
+
+open_rxh <- function(path = "/mnt/processed-data/disability/") {
+  list.files(path, 
+             pattern = "TAFRXH_\\d+\\.parquet", 
+             recursive = TRUE) |> 
+    (\(files) file.path(path, files))() |> 
+    open_dataset()
+}
+
+open_demo <- function(path = "/mnt/processed-data/disability/") {
+  list.files(path, 
+             pattern = "TAFDEBSE_\\d+\\.parquet", 
+             recursive = TRUE) |> 
+    (\(files) file.path(path, files))() |> 
+    open_dataset()
+}
+
+open_demc <- function(path = "/mnt/processed-data/disability") {
+  list.files(path, 
+             pattern = "TAFDEMC_\\d+\\.parquet", 
+             recursive = TRUE) |> 
+    (\(files) file.path(path, files))() |> 
+    open_dataset()
+}
